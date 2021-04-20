0ecfd0fb4f971c3629705350283ddfd0fc1783cf
Brhaka <martinez@brhaka.com>
2021-04-20 12:22:49 +0100

    Improvements to multiple object rendering

--- srcs/miniRT.c
+++ srcs/miniRT.c
@@ -6,7 +6,7 @@
 /*   By: ejuliao- <martinez@brhaka.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/08 15:12:09 by ejuliao-          #+#    #+#             */
-/*   Updated: 2021/04/20 10:39:24 by ejuliao-         ###   ########.fr       */
+/*   Updated: 2021/04/20 12:17:27 by ejuliao-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ void	init_holder(t_holder *holder)
 	holder->scene.cylinder = NULL;
 	holder->scene.triangle = NULL;
 	holder->scene.t_min = 0.0f;
-	holder->scene.t_max = 100000.0f;
+	holder->scene.t_max = FLT_MAX;
 }
 
 int	window_key_callback(int keycode, t_holder *holder)
