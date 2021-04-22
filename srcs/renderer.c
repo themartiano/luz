2eeae83b5378e4a5df00f4788a7359a3a775d8a2
Eduardo Julião Martinez <martinez@brhaka.com>
2021-04-22 12:17:01 +0100

    Added ray/light min_bounces and improvements

--- srcs/renderer.c
+++ srcs/renderer.c
@@ -6,14 +6,14 @@
 /*   By: ejuliao- <martinez@brhaka.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/14 11:55:19 by ejuliao-          #+#    #+#             */
-/*   Updated: 2021/04/22 11:56:26 by ejuliao-         ###   ########.fr       */
+/*   Updated: 2021/04/22 12:16:16 by ejuliao-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "miniRT.h"
 
 bool	check_ray_hits(t_holder *holder, t_ray ray, t_color *hit_color,
-t_hit_record *hit_record)
+t_hit_record *hit_rec)
 {
 	float	closest;
 	bool	hit;
@@ -22,12 +22,12 @@ t_hit_record *hit_record)
 	closest = holder->scene.t_max;
 	while (true)
 	{
-		if (hit_sphere(holder->scene, &ray, hit_record, closest))
+		if (hit_sphere(holder->scene, &ray, hit_rec, closest))
 		{
 			hit = true;
 			*hit_color = holder->scene.sphere->color;
-			gen_pixel_clr(holder, ray, hit_color, hit_record->t);
-			closest = hit_record->t;
+			gen_pixel_clr(holder, ray, hit_color, hit_rec->t);
+			closest = hit_rec->t;
 		}
 		if (holder->scene.sphere->next == NULL)
 			break ;
@@ -40,32 +40,36 @@ t_hit_record *hit_record)
 }
 
 void	light_bouncer(t_holder *holder, t_vec3 uv, t_color *tmp_color,
-t_hit_record *hit_record)
+t_hit_record *hit_rec)
 {
 	t_vec3	tmp_vec3;
 	t_vec3	target;
 	int		bounces;
+	int		attempts;
 
 	bounces = 0;
+	attempts = 0;
 	while (bounces < holder->scene.max_bounces)
 	{
-		target = random_in_unit();
-		tmp_vec3.x = hit_record->p.x + hit_record->normal.x + target.x;
-		tmp_vec3.y = hit_record->p.y + hit_record->normal.y + target.y;
-		tmp_vec3.z = hit_record->p.z + hit_record->normal.z + target.z;
-		target.x = tmp_vec3.x - hit_record->p.x;
-		target.y = tmp_vec3.y - hit_record->p.y;
-		target.z = tmp_vec3.z - hit_record->p.z;
+		tmp_vec3 = sum(sum(hit_rec->p, hit_rec->normal), random_in_unit());
+		target = sub(tmp_vec3, hit_rec->p);
 		if (!check_ray_hits(holder, gen_ray(holder->scene, uv,
-					hit_record->p, target), tmp_color, hit_record))
+					hit_rec->p, target), tmp_color, hit_rec))
 		{
-			break ;
+			if (bounces >= holder->scene.min_bounces
+				|| attempts >= holder->scene.min_bounces * 2)
+				break ;
+			else
+			{
+				attempts++;
+				continue ;
+			}
 		}
 		bounces++;
 	}
 }
 
-void	get_hit_color(t_holder *holder, t_hit_record *hit_record,
+void	get_hit_color(t_holder *holder, t_hit_record *hit_rec,
 t_color *tmp_color, t_vec3 crnt_pxl)
 {
 	t_vec3	uv;
@@ -75,15 +79,15 @@ t_color *tmp_color, t_vec3 crnt_pxl)
 	if (check_ray_hits(holder, gen_ray(holder->scene, uv,
 				holder->scene.camera.transform.position,
 				holder->scene.camera.transform.orientation), tmp_color,
-			hit_record))
+			hit_rec))
 	{
-		light_bouncer(holder, uv, tmp_color, hit_record);
+		light_bouncer(holder, uv, tmp_color, hit_rec);
 	}
 }
 
 void	render_loop(t_holder *holder, t_color *hit_color, int x, int y)
 {
-	t_hit_record	hit_record;
+	t_hit_record	hit_rec;
 	t_vec3			current_pixel;
 	t_color			tmp_color;
 	int				s;
@@ -94,7 +98,7 @@ void	render_loop(t_holder *holder, t_color *hit_color, int x, int y)
 	while (s < holder->scene.samples)
 	{
 		set_color(&tmp_color, 255, 255, 255);
-		get_hit_color(holder, &hit_record, &tmp_color, current_pixel);
+		get_hit_color(holder, &hit_rec, &tmp_color, current_pixel);
 		set_color(hit_color, hit_color->r + tmp_color.r,
 			hit_color->g + tmp_color.g, hit_color->b + tmp_color.b);
 		s++;
