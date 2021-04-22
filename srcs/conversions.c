2d9b6e74b4daf710a0438a005fb1bed91f81f5d7
Eduardo Martinez <git@martiano.me>
2021-04-22 17:41:40 +0100

    Working on real time rendering

--- srcs/conversions.c
+++ srcs/conversions.c
@@ -6,7 +6,7 @@
 /*   By: ejuliao- <martinez@brhaka.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/13 13:06:23 by ejuliao-          #+#    #+#             */
-/*   Updated: 2021/04/15 19:42:09 by ejuliao-         ###   ########.fr       */
+/*   Updated: 2021/04/22 15:46:49 by ejuliao-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,4 +33,33 @@ t_color	vec3_to_rgb(t_vec3 xyz)
 	color.b = xyz.z;
 	color.a = 0;
 	return (color);
+}
+
+int	rgba_to_hex(t_color rgba)
+{
+	if (rgba.r > 255)
+		rgba.r = 255;
+	if (rgba.g > 255)
+		rgba.g = 255;
+	if (rgba.b > 255)
+		rgba.b = 255;
+	rgba.a = 0;
+	return (rgba.a << 24 | rgba.r << 16 | rgba.g << 8 | rgba.b);
+}
+
+t_color	hex_to_rgba(int hex)
+{
+	t_color	rgba;
+
+	rgba.r = hex & (0xFF << 16);
+	rgba.g = hex & (0xFF << 8);
+	rgba.b = hex & 0xFF;
+	rgba.a = 0;
+	if (rgba.r > 255)
+		rgba.r = 255;
+	if (rgba.g > 255)
+		rgba.g = 255;
+	if (rgba.b > 255)
+		rgba.b = 255;
+	return (rgba);
 }
