52dde308d323722dfbf78d306a57af0963c0568c
Brhaka <martinez@brhaka.com>
2021-04-23 21:35:34 +0100

    Light now may be absorbed, depending on the hit color brightness

--- srcs/utils.c
+++ srcs/utils.c
@@ -6,7 +6,7 @@
 /*   By: ejuliao- <martinez@brhaka.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/10 14:51:57 by ejuliao-          #+#    #+#             */
-/*   Updated: 2021/04/23 11:39:03 by ejuliao-         ###   ########.fr       */
+/*   Updated: 2021/04/23 21:34:47 by ejuliao-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -73,8 +73,9 @@ t_vec3	unit_vector(t_vec3 vector)
 {
 	t_vec3	result;
 	float	k;
-	
-	k = 1.0f / sqrt(vector.x * vector.x + vector.y * vector.y + vector.z * vector.z);
+
+	k = 1.0f / sqrt(vector.x * vector.x + vector.y * vector.y
+			+ vector.z * vector.z);
 	result.x = vector.x * k;
 	result.y = vector.y * k;
 	result.z = vector.z * k;
