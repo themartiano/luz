8960f622881ff5428d70c747955d72a5f6b85dcc
Brhaka <martinez@brhaka.com>
2021-04-26 14:33:10 +0100

    Working on saving render to .bmp file

---
+++ includes/bmp.h
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   bmp.h                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ejuliao- <martinez@brhaka.com>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/26 11:13:09 by ejuliao-          #+#    #+#             */
+/*   Updated: 2021/04/26 12:07:17 by ejuliao-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BMP_H
+# define BMP_H
+
+# pragma pack(push, 1)
+
+typedef struct s_file_h
+{
+	int				type;
+	int				size;
+	int				reserved;
+	unsigned int	offset;
+}				t_file_h;
+
+typedef struct s_info_h
+{
+	unsigned int	size;
+	int				width;
+	int				height;
+	int				planes;
+	int				bits_per_pxl;
+	unsigned int	compression;
+	unsigned int	image_size;
+	int				pxls_per_meter_x;
+	int				pxls_per_meter_y;
+	unsigned int	color_used;
+	unsigned int	color_important;
+}				t_info_h;
+
+# pragma pack(pop)
+
+#endif
