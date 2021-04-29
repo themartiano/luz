8c49a8c742ef432d1ff6e6b7eeeba01509a90e92
Eduardo Martinez <git@martiano.me>
2021-04-29 19:38:00 +0100

    Working on cylinder rendering

--- srcs/cylinder_utils.c
+++ srcs/cylinder_utils.c
@@ -6,7 +6,7 @@
 /*   By: ejuliao- <martinez@brhaka.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/29 09:46:57 by ejuliao-          #+#    #+#             */
-/*   Updated: 2021/04/29 17:11:13 by ejuliao-         ###   ########.fr       */
+/*   Updated: 2021/04/29 19:26:35 by ejuliao-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,42 +20,55 @@ t_cylinder	*get_cylinder(t_scene *scene)
 	return (cylinder);
 }
 
-static void	plane(t_cylinder *cylinder, t_hit_record *hit_rec)
+static bool	plane(t_scene *scene, t_cylinder *cylinder, t_hit_record *hit_rec, float t_max)
 {
 	float t;
 
-	float a = dot(sub(hit_rec->p, cylinder->transform.position) ,cylinder->transform.orientation);
+	float a = dot(sub(hit_rec->p, cylinder->transform.position), cylinder->transform.orientation);
 	float b = dot(cylinder->transform.orientation, cylinder->transform.orientation);
-	if (b == 0 || (a < 0 && b < 0) || (a > 0 && b > 0))
-		return ;
+	if (b == 0.0f || (a < 0.0f && b < 0.0f) || (a > 0.0f && b > 0.0f))
+		return (false);
 	t = -a / b;
-	if (t < 0 || hit_rec->t < t)
-		return ;
+	if (t < scene->t_min || t > t_max)
+		return (false);
 	hit_rec->t = t;
 	hit_rec->normal = set((hit_rec->p.x
 				- cylinder->transform.position.x) / cylinder->radius,
 			(hit_rec->p.y - cylinder->transform.position.y)
-			/ cylinder->radius, (hit_rec->p.z
+			/ (cylinder->height / 2.0f), (hit_rec->p.z
 				- cylinder->transform.position.z) / cylinder->radius);
+	hit_rec->color = cylinder->color;
+	return (true);
 }
 
-static bool	intersect_cylinder(t_scene *scene, t_ray *ray, t_hit_record *hit_rec, float t)
+static bool	intersect_cylinder(t_scene *scene, t_ray *ray, t_hit_record *hit_rec, float t, float t_max)
 {
-	t_cylinder	*cylinder;
-	float		holder;
+	t_hit_record	hit_rec2;
+	t_cylinder		*cylinder;
 
 	cylinder = get_cylinder(scene);
-	hit_rec->t = t;
-	hit_rec->p = sum(ray->origin, mul(ray->direction, hit_rec->t));
-	holder = hit_rec->t;
-	plane(cylinder, hit_rec);
-	if (hit_rec->t <= cylinder->height / 2.0f)
-		return (true);
+	hit_rec2.p = sum(ray->origin, mul(ray->direction, t));
+	if (plane(scene, cylinder, &hit_rec2, t_max))
+		if (hit_rec2.t <= cylinder->height / 2.0f)
+		{
+			hit_rec->t = hit_rec2.t;
+			hit_rec->normal = hit_rec2.normal;
+			hit_rec->p = hit_rec2.p;
+			hit_rec->color = divide_color(sum_colors(hit_rec->color, hit_rec2.color), 2);
+			return (true);
+		}
 	cylinder->transform.orientation = mul(cylinder->transform.orientation, -1);
-	plane(cylinder, hit_rec);
-	if (hit_rec->t <= cylinder->height / 2.0f)
-		return (true);
-	hit_rec->t = holder;
+	if (plane(scene, cylinder, &hit_rec2, t_max))
+		if (hit_rec2.t <= cylinder->height / 2.0f)
+		{
+			cylinder->transform.orientation = mul(cylinder->transform.orientation, -1);
+			hit_rec->t = hit_rec2.t;
+			hit_rec->normal = hit_rec2.normal;
+			hit_rec->p = hit_rec2.p;
+			hit_rec->color = divide_color(sum_colors(hit_rec->color, hit_rec2.color), 2);
+			return (true);
+		}
+	cylinder->transform.orientation = mul(cylinder->transform.orientation, -1);
 	return (false);
 }
 
@@ -80,10 +93,10 @@ float t_max)
 	{
 		d = (-b - sqrt(tmp)) / a;
 		if (d < t_max && d > scene->t_min)
-			return (intersect_cylinder(scene, ray, hit_rec, d));
+			return (intersect_cylinder(scene, ray, hit_rec, d, t_max));
 		d = (-b + sqrt(tmp)) / a;
 		if (d < t_max && d > scene->t_min)
-			return (intersect_cylinder(scene, ray, hit_rec, d));
+			return (intersect_cylinder(scene, ray, hit_rec, d, t_max));
 	}
 	return (false);
 }
