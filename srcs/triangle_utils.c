6dc038f73eeabdfba7d6fd7a934148094541e4ef
Eduardo Martinez <git@martiano.me>
2021-05-14 11:51:56 +0100

    Small norm fixes

--- srcs/triangle_utils.c
+++ srcs/triangle_utils.c
@@ -6,7 +6,7 @@
 /*   By: ejuliao- <martinez@brhaka.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/10 09:59:05 by ejuliao-          #+#    #+#             */
-/*   Updated: 2021/05/14 11:42:18 by ejuliao-         ###   ########.fr       */
+/*   Updated: 2021/05/14 11:51:39 by ejuliao-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,24 +35,20 @@ float t_max)
 
 	v1 = sub(get_triangle(scene)->p2, get_triangle(scene)->p1);
 	v2 = sub(get_triangle(scene)->p3, get_triangle(scene)->p1);
-	t_vec3 p = cross(ray->direction, v2);
-	d = dot(v1, p);
+	d = dot(v1, cross(ray->direction, v2));
 	if (fabs(d) < scene->epsilon)
 		return (false);
 	t = sub(ray->origin, get_triangle(scene)->p1);
-	float tmp1 = 1.0f / d;
-	u = dot(t, p) * tmp1;
+	u = dot(t, cross(ray->direction, v2)) * (1.0f / d);
 	if (u < 0.0f || u > 1.0f || fabs(t.z) > t_max)
 		return (false);
-	t_vec3 q = cross(t, v1);
-	float v = dot(ray->direction, q) * tmp1;
-	if (v < 0.0f || u + v > 1.0f)
+	if (dot(ray->direction, cross(t, v1)) * (1.0f / d) < 0.0f || u
+		+ (dot(ray->direction, cross(t, v1)) * (1.0f / d)) > 1.0f)
 		return (false);
-	hit_rec->t = dot(v2, q) * tmp1;
-	hit_rec->p = sum(get_triangle(scene)->p1, sub(mul(v1, u), mul(v2, v)));
+	hit_rec->t = dot(v2, cross(t, v1)) * (1.0f / d);
+	hit_rec->p = sum(get_triangle(scene)->p1, sub(mul(v1, u), mul(v2,
+					dot(ray->direction, cross(t, v1)) * (1.0f / d))));
 	hit_rec->normal = normalize(cross(v1, v2));
-	//hit_rec->normal = mul(normalize(hit_rec->normal), -1.0f);
-	//printf("%f, %f, %f\n", hit_rec->normal.x, hit_rec->normal.y, hit_rec->normal.z);
 	hit_rec->hit_color = get_triangle(scene)->color;
 	return (true);
 }
