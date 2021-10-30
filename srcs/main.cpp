a2dda1a15e42076e6a1f09f2a435d71fd2b324d1
Brhaka <git@martiano.me>
2021-10-30 13:30:25 +0100

    Cornell Box on the main

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -31,71 +31,56 @@ int	main(int argc, char *argv[])
 
 	srand(time(0));
 
-	scene.setXResolution(1920);
-	scene.setYResolution(1080);
+	scene.setXResolution(500);
+	scene.setYResolution(500);
 	scene.initializePixelArray();
 	scene.setSampleCount(4);
-	scene.setMaxLightBounces(8);
+	scene.setMaxLightBounces(2);
 	scene.setGammaCorrected(true);
 	scene.setRenderSky(false);
 	scene.setBackgroundColor(Color(0.0f, 0.0f, 0.0f));
 
-
 	// Current coordinate system ~~ Forward: -Z | Up: -Y | Right: -X
-	scene.addCamera(Camera(Vector3(0.0f, 0.0f, 12.0f), Vector3(0.0f, 0.0f, -8.0f), 65, 0.1856321f));
 
-	// Emissive rectangle
-	scene.addHittable(std::make_shared<Rectangle>(Transform(Vector3(-5.0f, -3.0f, -8.0f), Vector3(0.68f, -0.3f, 1.0f), Vector3(1.0f, 1.0f, 1.0f)), 5.0f, Material(Color(1.0f, 1.10f, 1.0f), 1.0f, 0.0f, 0.5f, 0.0f, false, true, 10.0f)));
+	// Cornell Box
+	scene.addCamera(Camera(Vector3(0.0f, 0.0f, 390.0f), Vector3(0.0f, 0.0f, -250.0f), 65, 0.18f));
 
-	// Glass
-	scene.addHittable(std::make_shared<Sphere>(Vector3(-6.7f, 0.0f, -4.0f), Material(Color(0.0f, 0.0f, 0.0f), 1.0f, 0.0f, 0.5f, 0.0f, true, false, 0.0f), 3.0f));
-	// Metal
-	scene.addHittable(std::make_shared<Sphere>(Vector3(-3.35f, 0.0f, -8.0f), Material(Color(0.7f, 0.6f, 0.5f), 1.0f, 1.0f, 0.5f, 0.0f, false, false, 0.0f), 3.0f));
 
-	// Emissive sphere
-	//scene.addHittable(std::make_shared<Sphere>(Vector3(-3.35f, 0.0f, -8.0f), Material(Color(1.0f, 1.0f, 1.0f), 1.0f, 0.0f, 0.5f, 0.0f, false, true, 1.0f), 3.0f));
+	// Top Wall
+	scene.addHittable(std::make_shared<Rectangle>(
+		Transform(Vector3(0.0f, -250.0f, -250.0f), Vector3(0.0f, 1.0f, 0.0f), Vector3(1.0f, 1.0f, 1.0f)),
+		500.0f,
+		Material(Color(1.0f, 1.0f, 1.0f), 1.0f, 0.0f, 0.5f, 0.0f, false, false, 0.0f)));
 
-	// Lambertian
-	scene.addHittable(std::make_shared<Sphere>(Vector3(0.0f, 0.0f, -12.0f), Material(Color(0.0f, 0.0f, 0.8f), 1.0f, 0.0f, 0.5f, 0.0f, false, false, 0.0f), 3.0f));
+	// Light (Top)
+	scene.addHittable(std::make_shared<Rectangle>(
+		Transform(Vector3(0.0f, -250.0f, -125.0f), Vector3(0.0f, 1.0f, 0.0f), Vector3(1.0f, 1.0f, 1.0f)),
+		125.0f,
+		Material(Color(1.0f, 1.0f, 1.0f), 1.0f, 0.0f, 0.5f, 0.0f, false, true, 10.0f)));
 
-	// Ground (Plane)
-	scene.addHittable(std::make_shared<Plane>(3.0f, Vector3(0.0f, -1.0f, 0.0f), Material(Color(0.6f, 0.6f, 0.6f), 1.0f, 0.0f, 0.5f, 0.0f, false, false, 0.0f)));
+	// Front Wall
+	scene.addHittable(std::make_shared<Rectangle>(
+		Transform(Vector3(0.0f, 0.0f, -250.0f), Vector3(0.0f, 0.0f, 1.0f), Vector3(1.0f, 1.0f, 1.0f)),
+		500.0f,
+		Material(Color(1.0f, 1.0f, 1.0f), 1.0f, 0.0f, 0.5f, 0.0f, false, false, 0.0f)));
 
-	/*std::vector<std::shared_ptr<Hittable>> tinySpheres;
-	for (int x = -11; x < 11; x++)
-	{
-		for (int y = -11; y < 11; y++)
-		{
-			if (x == 0)
-				break;
-			if (y == 0)
-				continue;
+	// Floor Wall
+	scene.addHittable(std::make_shared<Rectangle>(
+		Transform(Vector3(0.0f, 250.0f, -250.0f), Vector3(0.0f, -1.0f, 0.0f), Vector3(1.0f, 1.0f, 1.0f)),
+		500.0f,
+		Material(Color(1.0f, 1.0f, 1.0f), 1.0f, 0.0f, 0.5f, 0.0f, false, false, 0.0f)));
 
-			float	sphereRadius = randomFloat() / 2.0f;
+	// Right Wall
+	scene.addHittable(std::make_shared<Rectangle>(
+		Transform(Vector3(-250.0f, 0.0f, -250.0f), Vector3(1.0f, 0.0f, 0.0f), Vector3(1.0f, 1.0f, 1.0f)),
+		500.0f,
+		Material(Color(1.0f, 0.0f, 0.0f), 1.0f, 0.0f, 0.5f, 0.0f, false, false, 0.0f)));
 
-			Vector3 spherePosition(float(x) * (3.6f * randomFloat()), 3.0f - sphereRadius, float(y) * (3.0f * randomFloat()));
-
-			float	random = randomFloat();
-
-			float	metallic = 0.0f;
-			float	reflectionFuzziness = 0.0f;
-			bool	glass = false;
-
-			if (random >= 0.85f && random < 0.95f)
-			{
-				metallic = 1.0f;
-				reflectionFuzziness = randomFloat() - 0.5f;
-				reflectionFuzziness = reflectionFuzziness < 0.0f ? 0.0f : reflectionFuzziness;
-			}
-			else if (random >= 0.95f)
-			{
-				glass = true;
-			}
-
-			tinySpheres.push_back(std::make_shared<Sphere>(spherePosition, Material(Color(randomFloat(), randomFloat(), randomFloat()), 1.0f, metallic, 0.5f, reflectionFuzziness, glass), sphereRadius));
-		}
-	}
-	scene.addHittable(std::make_shared<BVHNode>(tinySpheres));*/
+	// Left Wall
+	scene.addHittable(std::make_shared<Rectangle>(
+		Transform(Vector3(250.0f, 0.0f, -250.0f), Vector3(-1.0f, 0.0f, 0.0f), Vector3(1.0f, 1.0f, 1.0f)),
+		500.0f,
+		Material(Color(0.0f, 1.0f, 0.0f), 1.0f, 0.0f, 0.5f, 0.0f, false, false, 0.0f)));
 
 	render(scene);
 
