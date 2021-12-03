7764e6c5d2054598b428c5d9a835a68c101569f6
Brhaka <git@martiano.me>
2021-12-03 16:05:38 +0000

    Atmosphere improvements
    View from space and distant objects

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -31,31 +31,38 @@ int	main(void)
 	scene.setXResolution(500);
 	scene.setYResolution(500);
 	scene.initializePixelArray();
-	scene.setSampleCount(1);
-	scene.setMaxLightBounces(6);
+	scene.setSampleCount(200);
+	scene.setMaxLightBounces(50);
 	scene.setGammaCorrected(true);
-	scene.setRenderSky(SKY_NONE);
-	//scene.setAtmosphere(Atmosphere(0.0, D_EARTH_RADIUS, D_ATMOSPHERE_RADIUS, D_HR, D_HM, 64, 32, 0.268)); // Only needed if Scene.Sky == SKY_ATMOSPHERE
-	scene.setBackgroundColor(Color(1.0, 1.0, 1.0)); // Only needed if Scene.Sky == SKY_NONE
+	scene.setRenderSky(SKY_ATMOSPHERE);
+	scene.setAtmosphere(Atmosphere(-0.189, D_EARTH_RADIUS, D_ATMOSPHERE_RADIUS, D_HR, D_HM, 64, 32, 0.468)); // Only needed if Scene.Sky == SKY_ATMOSPHERE
+	//scene.setBackgroundColor(Color(1.0, 1.0, 1.0)); // Only needed if Scene.Sky == SKY_NONE
 
 	// Coordinate system ~~ Right Hand ~~ Forward: -Z | Up: +Y | Right: +X
 
 	//mountCornellBox(scene);
 
-	scene.addCamera(Camera(Vector3(0.0, 0.0, 5.0), Vector3(0.0, 0.0, -1.0), 65, 0.0));
+	scene.addCamera(Camera(Vector3(0.0, 0.0, D_ATMOSPHERE_RADIUS + 10000000), Vector3(0.0, 0.0, 1.0), 65, 0.0));
+	//scene.addCamera(Camera(Vector3(0.0, D_EARTH_RADIUS, 0.0), Vector3(0.0, 0.0, 1.0), 65, 0.0));
 
 	// scene.addHittable(std::make_shared<Plane>(
-	// 	-1.0,
+	// 	D_EARTH_RADIUS,
 	// 	Vector3(0.0, 1.0, 0.0),
+	// 	Material(Color(0.0, 1.0, 0.0), 1.0, 0.0, 0.5, 0.0, false, false, 0.0)
+	// ));
+
+	// scene.addHittable(std::make_shared<Triangle>(
+	// 	Vector3(-1.0, 0.0, 0.0),
+	// 	Vector3(1.0, 0.0, 0.0),
+	// 	Vector3(-2.0, 2.0, 0.0),
 	// 	Material(Color(1.0, 1.0, 1.0), 1.0, 0.0, 0.5, 0.0, false, false, 0.0)
 	// ));
 
-	scene.addHittable(std::make_shared<Triangle>(
-		Vector3(-1.0, 0.0, 0.0),
-		Vector3(1.0, 0.0, 0.0),
-		Vector3(-2.0, 2.0, 0.0),
-		Material(Color(1.0, 1.0, 1.0), 1.0, 0.0, 0.5, 0.0, false, false, 0.0)
-	));
+	// scene.addHittable(std::make_shared<Sphere>(
+	// 	Vector3(0.0, 0.0, 0.0),
+	// 	Material(Color(0.0, 1.0, 0.0), 1.0, 0.0, 0.5, 0.0, false, false, 0.0),
+	// 	D_EARTH_RADIUS
+	// ));
 
 	//readObj(scene, "blender_cube");
 
