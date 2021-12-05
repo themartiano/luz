0f1a2f46c4997dcce6add4d6f749ab1481403b2b
Brhaka <martinez@brhaka.com>
2021-12-05 16:13:15 +0000

    Improvements to the Scene file parser
    General improvements and Camera parsing

--- srcs/Camera.cpp
+++ srcs/Camera.cpp
@@ -30,10 +30,22 @@ Vector3		Camera::getPosition(void) const
 	return (this->_position);
 }
 
+// Sets the Camera's position
+void	Camera::setPosition(Vector3 position)
+{
+	this->_position = position;
+}
+
 // Returns the Camera's direction (facing direction, look direction)
 Vector3		Camera::getDirection(void) const
 {
 	return (this->_direction);
+}
+
+// Sets the Camera's direction
+void	Camera::setDirection(Vector3 direction)
+{
+	this->_direction = direction;
 }
 
 // Returns the Camera's FOV (Field of View)
@@ -42,14 +54,32 @@ short		Camera::getFOV(void) const
 	return (this->_fov);
 }
 
+// Sets the Camera's FOV
+void	Camera::setFOV(short fov)
+{
+	this->_fov = fov;
+}
+
 // Returns the Camera's Aperture
 double	Camera::getAperture(void) const
 {
 	return (this->_aperture);
 }
 
+// Sets the Camera's aperture
+void	Camera::setAperture(double aperture)
+{
+	this->_aperture = aperture;
+}
+
 // Returns the Camera's Focus Distance (used for Depth of Field / DOF)
 double	Camera::getFocusDistance(void) const
 {
 	return (this->_focusDistance);
+}
+
+// Sets the Camera's Focus Distance
+void	Camera::setFocusDistance(double focusDistance)
+{
+	this->_focusDistance = focusDistance;
 }
--- srcs/SceneFile.cpp
+++ srcs/SceneFile.cpp
@@ -1,9 +1,13 @@
 #include "SceneFile.hpp"
 #include "ANSIColors.hpp"
 #include "Utilities.hpp"
+#include "Vector3.hpp"
 #include <fstream>
+#include <stdio.h>
 
 static void	readSettingsSection(Scene& scene, std::ifstream& stream);
+static void	readSceneSection(Scene& scene, std::ifstream& stream);
+static void	readObjectsSubSection(Scene& scene, std::ifstream& stream);
 
 // Searches and reads / parses the Scene file named 'fileName' into 'Scene' (searches in the current directory)
 void   readSceneFile(Scene& scene, std::string fileName)
@@ -33,7 +37,7 @@ void   readSceneFile(Scene& scene, std::string fileName)
         }
         else if (line.rfind("[scene]", 0) != std::string::npos)
         {
-            //readSceneSection(scene, stream);
+            readSceneSection(scene, stream);
         }
         else if (line.rfind("[materials]", 0) != std::string::npos)
         {
@@ -52,27 +56,120 @@ static void	readSettingsSection(Scene& scene, std::ifstream& stream)
 
 		if (line.length() <= 0)
 		{
-			continue;
+			break;
 		}
         Utilities::toLower(line);
 
         if (line.rfind("resolution=", 0) != std::string::npos)
         {
-            std::string temp = line.substr(line.find('=') + 1);
-            scene.setXResolution(std::stoi(temp.substr(0, temp.find(','))));
-            scene.setYResolution(std::stoi(temp.substr(temp.find(',') + 1)));
+            double x, y;
+
+            if (sscanf(line.c_str(), "resolution=%lf,%lf\n", &x, &y) != EOF)
+            {
+                scene.setXResolution(x);
+                scene.setYResolution(y);
+            }
         }
         else if (line.rfind("samples=", 0) != std::string::npos)
         {
-            scene.setSampleCount(std::stoi(line.substr(line.find('=') + 1)));
+            int samples;
+
+            if (sscanf(line.c_str(), "samples=%d\n", &samples) != EOF)
+            {
+                scene.setSampleCount(samples);
+            }
         }
         else if (line.rfind("maxlightbounces=", 0) != std::string::npos)
         {
-            scene.setMaxLightBounces(std::stoi(line.substr(line.find('=') + 1)));
+            int maxLightBounces;
+
+            if (sscanf(line.c_str(), "maxlightbounces=%d\n", &maxLightBounces) != EOF)
+            {
+                scene.setMaxLightBounces(maxLightBounces);
+            }
         }
         else if (line.rfind("gamma=", 0) != std::string::npos)
         {
-            scene.setGammaCorrected(line.substr(line.find('=') + 1) == "true" ? true : false);
+            int gammaCorrected;
+
+            if (sscanf(line.c_str(), "gamma=%d\n", &gammaCorrected) != EOF)
+            {
+                scene.setGammaCorrected(gammaCorrected);
+            }
+        }
+	} while (!stream.eof());
+}
+
+// Parses the [scene] section of a Scene file
+static void	readSceneSection(Scene& scene, std::ifstream& stream)
+{
+	std::string line;
+	do
+	{
+		getline(stream, line);
+
+		if (line.length() <= 0)
+		{
+			break;
+		}
+        Utilities::toLower(line);
+
+        if (line.rfind("camera=", 0) != std::string::npos)
+        {
+            double pX, pY, pZ, dX, dY, dZ, aperture, focusDistance;
+            int fov;
+
+            if (sscanf(line.c_str(), "camera=(%lf,%lf,%lf),(%lf,%lf,%lf),%d,%lf,%lf\n", &pX, &pY, &pZ, &dX, &dY, &dZ, &fov, &aperture, &focusDistance) != EOF)
+            {
+                Camera camera;
+
+                camera.setPosition(Vector3(pX, pY, pZ));
+                camera.setDirection(Vector3(dX, dY, dZ));
+                camera.setFOV(fov);
+                camera.setAperture(aperture);
+                camera.setFocusDistance(focusDistance);
+
+                scene.addCamera(camera);
+            }
+        }
+        else if (line.rfind("objects{", 0) != std::string::npos)
+        {
+            readObjectsSubSection(scene, stream);
+        }
+	} while (!stream.eof());
+}
+
+// Parses the 'objects' sub-section of a Scene file
+static void	readObjectsSubSection(Scene& scene, std::ifstream& stream)
+{
+	std::string line;
+	do
+	{
+		getline(stream, line);
+
+		if (line.length() <= 0)
+		{
+			continue;
+		}
+        if (line == "}")
+		{
+			break;
+		}
+        Utilities::toLower(line);
+
+        if (line.rfind("sphere=", 0) != std::string::npos)
+        {
+            double pX, pY, pZ, radius;
+            std::string materialName;
+
+            if (sscanf(line.c_str(), "sphere=(%lf,%lf,%lf),%s,%lf\n", &pX, &pY, &pZ, &materialName.c_str(), &radius) != EOF)
+            {
+                Sphere sphere;
+
+                sphere.
+
+                scene.addHittable(sphere);
+            }
         }
 	} while (!stream.eof());
 }
