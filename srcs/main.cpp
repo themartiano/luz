a61290528a3b1fae6fd98dd288f06dcfb2dff2e3
Brhaka <git@martiano.me>
2022-01-06 19:56:20 +0000

    Fixes or gambiarras on the coordinates (ray generation)

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -35,19 +35,19 @@ int	main(int argc, char *argv[])
 		// Coordinate system ~~ Right Hand ~~ Forward: -Z | Up: +Y | Right: +X
 
 		//scene.addCamera(Camera(Vector3(5.0, 0.0, 0.0), Vector3(-1.0, 0.0, 0.0), 65, 0.0, 1.0));
-		scene.addCamera(Camera(Vector3(0.0, 0.0, -5.0), Vector3(0.0, 0.0, 1.0), 65, 0.0, 1.0));
+		scene.addCamera(Camera(Vector3(0.0, 0.0, 5.0), Vector3(0.0, 0.0, -1.0), 65, 0.0, 1.0));
 
-		readObj(scene, "objects/cube.obj");
+		// readObj(scene, "objects/cube.obj");
 
-		// scene.addHittable(std::make_shared<Triangle>(
-		// 	Vector3(0, 0, 0),
-		// 	Vector3(0, 1, 0),
-		// 	Vector3(1, 0, 0),
-		// 	Material(Color(0.6, 1.0, 0.6), 1.0, 0.0, 0.5, 0.0, false, false, 0.0)
-		// ));
+		scene.addHittable(std::make_shared<Triangle>(
+			Vector3(0, 0, 0),
+			Vector3(0, 1, 0),
+			Vector3(1, 0, 0),
+			Material(Color(0.6, 1.0, 0.6), 1.0, 0.0, 0.5, 0.0, false, false, 0.0)
+		));
 
 		// scene.addHittable(std::make_shared<Sphere>(
-		// 	Vector3(0, 0, 0),
+		// 	Vector3(1, 1, 1),
 		// 	2,
 		// 	Material(Color(0.6, 0.6, 1.0), 1.0, 0.0, 0.5, 0.0, false, false, 0.0)
 		// ));
