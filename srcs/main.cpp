539d2002f2f255bc50d5c83e8b1ea99f73bfa540
Brhaka <martinez@brhaka.com>
2022-01-11 19:03:18 +0000

    Sphere hit technical improvements and working on triangle rendering

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -26,7 +26,7 @@ int	main(int argc, char *argv[])
 	{
 		scene.setXResolution(500);
 		scene.setYResolution(500);
-		scene.setSampleCount(10);
+		scene.setSampleCount(1);
 		scene.setMaxLightBounces(8);
 		scene.setGammaCorrected(true);
 		scene.setRenderSky(SKY_NONE);
@@ -36,9 +36,9 @@ int	main(int argc, char *argv[])
 		// Coordinate system ~~ Right Hand ~~ Forward: -Z | Up: +Y | Right: +X
 
 		//scene.addCamera(Camera(Vector3(5.0, 0.0, 0.0), Vector3(-1.0, 0.0, 0.0), 65, 0.0, 1.0));
-		scene.addCamera(Camera(Vector3(0.0, 1.0, 5.0), Vector3(0.0, 0.0, -1.0), 65, 0.0, 1.0));
+		scene.addCamera(Camera(Vector3(0.0, 2.0, 12.0), Vector3(0.0, 0.0, -1.0), 65, 0.0, 1.0));
 
-		//readObj(scene, "objects/cube.obj");
+		readObj(scene, "objects/lamp.obj");
 
 		// scene.addHittable(std::make_shared<Triangle>(
 		// 	Vector3(1, 0, 0),
@@ -54,17 +54,17 @@ int	main(int argc, char *argv[])
 		// 	Material(Color(0.6, 0.6, 1.0), 1.0, 0.0, 0.5, 0.0, false, false, 0.0)
 		// ));
 
-		scene.addHittable(std::make_shared<Sphere>(
-			Vector3(1, 1, 1),
-			2,
-			Material(Color(0.6, 0.6, 1.0), 1.0, 0.0, 0.5, 0.0, false, false, 0.0)
-		));
+		// scene.addHittable(std::make_shared<Sphere>(
+		// 	Vector3(1, 1, 1),
+		// 	2,
+		// 	Material(Color(0.6, 0.6, 1.0), 1.0, 0.0, 0.5, 0.0, false, false, 0.0)
+		// ));
 
-		scene.addHittable(std::make_shared<Plane>(
-			-1.0,
-			Vector3(0.0, 1.0, 0.0),
-			Material(Color(0.6, 0.6, 0.6), 1.0, 0.0, 0.5, 0.0, false, false, 0.0)
-		));
+		// scene.addHittable(std::make_shared<Plane>(
+		// 	-1.0,
+		// 	Vector3(0.0, 1.0, 0.0),
+		// 	Material(Color(0.6, 0.6, 0.6), 1.0, 0.0, 0.5, 0.0, false, false, 0.0)
+		// ));
 	}
 
 	if (Renderer::render(scene))
