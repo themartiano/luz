98b14e5ad5ca34f5814d69464b4f02aa09bb7ab8
Brhaka <git@martiano.me>
2022-04-16 00:44:31 +0100

    Landscape rendering fixes & improvements

--- includes/Hittables/Landscape.hpp
+++ includes/Hittables/Landscape.hpp
@@ -7,7 +7,7 @@ class	Landscape : public Hittable
 {
 	public:
 		Landscape(void);
-		Landscape(std::shared_ptr<Material> material, double noiseScale, unsigned int seed, unsigned int samplesPerRay);
+		Landscape(Vector3 position, double size, std::shared_ptr<Material> material, unsigned int subSamples, double noiseScale, unsigned int seed);
 		virtual std::shared_ptr<Material>	getMaterial(void) const override;
 		virtual bool		hit(Ray& ray, HitRecord& hitRecord, double t_min, double t_max) const override;
 		virtual bool		createBoundingBox(AABB& outputBoundingBox) const override;
@@ -16,11 +16,12 @@ class	Landscape : public Hittable
 		double	_getHeightAtPoint(double x, double z) const;
 		Vector3	_getNormalAtPosition(Vector3 position, double t_min) const;
 
+		Vector3			_position;
+		double			_size;
 		std::shared_ptr<Material>	_material;
+		unsigned int	_subSamples;
 		double			_noiseScale; // The scale of the Perlin noise
 		unsigned int	_seed;
-		unsigned int	_samplesPerRay;
-		double			_size;
 
 		Perlin			_perlin;
 };
